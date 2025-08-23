f689c4d302b997a9d3bd8524fa6a5b6ade9d0adf
Seva <68725282+sevcator@users.noreply.github.com>
2025-08-24 00:03:52 +0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -5,14 +5,13 @@ execute_command() {
     fi
 }
 
-execute_command apt-get -y -o Dpkg::Options::=--force-confnew update
-execute_command apt-get -y -o Dpkg::Options::=--force-confnew upgrade
-execute_command apt-get -y -o Dpkg::Options::=--force-confnew install root-repo
-execute_command apt-get -y -o Dpkg::Options::=--force-confnew install tsu
-execute_command apt-get -y -o Dpkg::Options::=--force-confnew install python
-execute_command apt-get -y -o Dpkg::Options::=--force-confnew install wpa-supplicant
-execute_command apt-get -y -o Dpkg::Options::=--force-confnew install pixiewps
-execute_command apt-get -y -o Dpkg::Options::=--force-confnew install iw
-execute_command apt-get -y -o Dpkg::Options::=--force-confnew install openssl
+execute_command pkg -y -o Dpkg::Options::=--force-confnew update
+execute_command pkg -y -o Dpkg::Options::=--force-confnew upgrade
+execute_command pkg -y -o Dpkg::Options::=--force-confnew install root-repo
+execute_command pkg -y -o Dpkg::Options::=--force-confnew install tsu
+execute_command pkg -y -o Dpkg::Options::=--force-confnew install python
+execute_command pkg -y -o Dpkg::Options::=--force-confnew install wpa-supplicant
+execute_command pkg -y -o Dpkg::Options::=--force-confnew install pixiewps
+execute_command pkg -y -o Dpkg::Options::=--force-confnew install iw
 
 execute_command curl -L -o oneshot.py https://raw.githubusercontent.com/sevcator/oneshot-termux/refs/heads/master/oneshot.py
