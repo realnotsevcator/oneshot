b5c555e10196f25745badb67d0c977a9255a7514
Seva <68725282+sevcator@users.noreply.github.com>
2025-08-23 16:05:50 +0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -5,13 +5,13 @@ execute_command() {
     fi
 }
 
-execute_command pkg update -y
-execute_command pkg install -y root-repo
-execute_command pkg install -y tsu
-execute_command pkg install -y python
-execute_command pkg install -y wpa-supplicant
-execute_command pkg install -y pixiewps
-execute_command pkg install -y iw
-execute_command pkg install -y openssl
-execute_command curl -L -o https://raw.githubusercontent.com/sevcator/oneshot-termux/refs/heads/master/oneshot.py
-  
+execute_command apt-get update
+execute_command apt-get -y -o Dpkg::Options::=--force-confnew install root-repo
+execute_command apt-get -y -o Dpkg::Options::=--force-confnew install tsu
+execute_command apt-get -y -o Dpkg::Options::=--force-confnew install python
+execute_command apt-get -y -o Dpkg::Options::=--force-confnew install wpa-supplicant
+execute_command apt-get -y -o Dpkg::Options::=--force-confnew install pixiewps
+execute_command apt-get -y -o Dpkg::Options::=--force-confnew install iw
+execute_command apt-get -y -o Dpkg::Options::=--force-confnew install openssl
+
+execute_command curl -L -o oneshot.py https://raw.githubusercontent.com/sevcator/oneshot-termux/refs/heads/master/oneshot.py
