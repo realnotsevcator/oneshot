57996fee70fded3cd54d2c22120fba4cfd2fdeb7
Seva <68725282+sevcator@users.noreply.github.com>
2025-08-24 12:41:27 +0700

    Update termux-installer.sh

--- termux-installer.sh
+++ termux-installer.sh
@@ -8,7 +8,7 @@ execute_command() {
 execute_command pkg update -y -o Dpkg::Options::=--force-confnew
 execute_command pkg upgrade -y -o Dpkg::Options::=--force-confnew
 execute_command pkg install root-repo -y -o Dpkg::Options::=--force-confnew
-execute_command pkg install tsu -y -o Dpkg::Options::=--force-confnew
+execute_command pkg install sudo -y -o Dpkg::Options::=--force-confnew
 execute_command pkg install python -y -o Dpkg::Options::=--force-confnew
 execute_command pkg install wpa-supplicant -y -o Dpkg::Options::=--force-confnew
 execute_command pkg install pixiewps -y -o Dpkg::Options::=--force-confnew
